d3471d4e0b1b9089ea3cee5cfeca40223c1fcb93
abrikos <abrikoz@gmail.com>
2016-11-12 20:34:34 +0900

    version 1.01

--- views/update/pull.php
+++ views/update/pull.php
@@ -2,6 +2,9 @@
 <div>
 	<code><?=$result?></code>
 </div>
-<?=$info?>
+<?php
+preg_match('!\n\n(.*)\n\n!',$info,$arr);
+print_r($arr)
+?>
 <hr/>
 При необходимости сфотографируйте это и отправьте разработчикам
